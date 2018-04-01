85fbc6d97df151e6113d548c28641878e159ffd8
Pablo Victor <pablo.santos@ccc.ufcg.edu.br>
2018-03-31 23:18:37 -0300

    Adding dashboard styles

--- assets/app/app/core/core-routing.module.ts
+++ assets/app/app/core/core-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-  import {RouterModule, Routes} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
 import {AuthGuard} from "./services/auth.guard";
 import {SignUpComponent} from "../signup/signup.component";
 import {LoginComponent} from "./login/login.component";
