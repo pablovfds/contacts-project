7e149bd19003fe245650bf3a66833b38baeceb10
Pablo Victor <pablo.santos@ccc.ufcg.edu.br>
2018-04-13 11:44:58 -0300

    implementing the user profile page

--- assets/app/app/admin/admin-routing.module.ts
+++ assets/app/app/admin/admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,13 @@ const routes: Routes = [
     component: AdminComponent,
     children: [
       { path: '', component: DashboardComponent }
+    ]
+  },
+  {
+    path: 'profile',
+    component: AdminComponent,
+    children: [
+      { path: '', component: ProfileComponent }
     ]
   }
 ];
--- assets/app/app/admin/admin.module.ts
+++ assets/app/app/admin/admin.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: [AdminComponent, DashboardComponent]
+  declarations: [AdminComponent, DashboardComponent, ProfileComponent]
 })
 export class AdminModule { }
